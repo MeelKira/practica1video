425c8a11592fa36e39489b70a053b2d23bb1196f
Kiragames96 <meel.assassin@gmail.com>
2022-08-14 23:11:11 -0500

    video

---
+++ app/src/main/java/com/example/notifi_multi/Video1.java
@@ -0,0 +1,14 @@
+package com.example.notifi_multi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Video1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video1);
+    }
+}
